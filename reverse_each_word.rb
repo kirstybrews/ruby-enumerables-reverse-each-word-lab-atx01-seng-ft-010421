142b8c445e6ff1cba3dce8a429f7d78cbf83208b
Kirsty Brewster <kirstybrewster95@gmail.com>
2020-09-02 20:06:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(sentence)
   sentence = sentence.split
   sentence.collect do |string|
-    string.reverse
+    string.reverse.join(" ")
   end
-  return sentence.join(" ")
 end
